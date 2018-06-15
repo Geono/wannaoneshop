0e7776a795b10336722569d3fae35468e110acad
SUSEOK LEE <suseok110.lee@gmail.com>
2018-06-15 16:44:37 +0900

    Add Spring Project

---
+++ src/test/java/kr/ebaykorea/wannaoneshop/WannaoneshopApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.ebaykorea.wannaoneshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WannaoneshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
