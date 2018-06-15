76a8a9e4dba0c461943b09a164784ba2665d3ea4
Kunho Kim <kunkim@ebay.com>
2018-06-15 16:51:23 +0900

    Copied jpaboard into here

---
+++ jpaboard/src/main/java/examples/boot/jpaboard/controller/MainController.java
@@ -0,0 +1,13 @@
+package examples.boot.jpaboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    // resources/templates/main.html;
+    @GetMapping(path="/")
+    public String main() {
+        return "main";
+    }
+}
