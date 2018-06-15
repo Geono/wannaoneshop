f5e9d9f758f25fc34220a0a7c9538e6d842dcfa2
Kunho Kim <kunkim@ebay.com>
2018-06-15 17:06:18 +0900

    Delete original jpaboard

--- README.md
+++
@@ -1,2 +0,0 @@
-# WannaOne shop
-
--- jpaboard/src/main/java/examples/boot/jpaboard/repository/MemberRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package examples.boot.jpaboard.repository;
-
-import examples.boot.jpaboard.domain.MemberRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRoleRepository
-        extends JpaRepository<MemberRole, Long> {
-
-}
